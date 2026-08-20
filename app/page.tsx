299ee8bf8160d7a9c67c4cdabf563dfc2bed2814
Haneen Zibar <haneenzibar@gmail.com>
2026-08-20 17:06:22 +0300

    Save my local changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Homepage from "./Homepage/page";
 import Personalinform from "./components/Personalinform";
+// import GoalsForm from "./components/GoalsForm"
 
 export default function Home() {
   return (
     <main>
       <Homepage />
         <Personalinform />
+        {/* <GoalsForm /> */}
       {/* rest of your homepage content goes here */}
     </main>
   );
